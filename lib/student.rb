b73f760487a30b6a7f03861d14ba4dc11838171a
Mac Dowell Prosper <macdowell@noukod.com>
2018-11-08 04:19:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,14 +26,19 @@ class Student
        update
     else
       sql = <<-SQL
-         INSERT INTO students (name, grade) 
+         INSERT INTO students (name, grade)
          VALUES (?, ?)
       SQL
 
       DB[:conn].execute(sql, name, grade)
 
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-    end   
+    end
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
