e8c99cd29daa0a7b0fe5de767a494fa91869ec97
Mac Dowell Prosper <macdowell@noukod.com>
2018-11-08 04:20:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
 
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
+    DB[:conn].execute(sql, name, grade, id)
   end
 
 end
