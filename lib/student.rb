bf603def4bf576b9fe246dd46fbed317584ca2c0
Mac Dowell Prosper <macdowell@noukod.com>
2018-11-08 04:13:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,5 +9,13 @@ class Student
       @grade = grade
   end
 
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
+    SQL
+    DB[:conn].execute(sql) 
+  end
+
 
 end
